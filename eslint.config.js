4396bd54483a2ab6720d2b6e43e63311592a9b4b
pv18 <pv9998286620@yandex.ru>
2024-08-21 11:04:40 +0300

    add eslint, prettier, stylelint, lint-staged and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintReact from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
   files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
+  ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
   },
   plugins: {
+    react: eslintReact,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
+    prettier: prettierPlugin,
   },
   rules: {
+    ...prettierPlugin.configs.recommended.rules,
+    ...eslintConfigPrettier.rules,
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react/no-unknown-property': 'warn',
+    'no-extra-boolean-cast': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-duplicate-case': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'no-undef': 'warn',
+    'no-empty': 'warn',
+    'no-unsafe-optional-chaining': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-})
+});
+
